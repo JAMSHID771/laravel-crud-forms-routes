57c04d82308a4a739a3a2eda63f63ee8b11304ba
Jamshid <jmamataliyev111@gmail.com>
2025-03-14 12:01:23 +0500

    the URL has been removed

--- resources/views/posts/form.blade.php
+++ resources/views/posts/form.blade.php
@@ -1,32 +1,28 @@
-
-  <form method="GET" action="{{ url('/posts') }}">
-        @csrf
-        <input type="text" name="get_input" placeholder="GET">
-        <button type="submit">Yuborish</button>
-    </form>
-    <form method="POST" action="{{ url('/posts/create') }}">
-        @csrf
-        <input type="text" name="post_input" placeholder="POST">
-        <button type="submit">Yuborish</button>
-    </form>
-    <form method="POST" action="{{ url('/posts/delete') }}">
-        @csrf
-        @method('DELETE')
-        <input type="text" name="delete_input" placeholder="DELETE">
-        <button type="submit">Yuborish</button>
-    </form>
-    <form method="POST" action="{{ url('/posts/edit') }}">
-        @csrf
-        @method('PATCH')
-        <input type="text" name="patch_input" placeholder="PATCH">
-        <button type="submit">Yuborish</button>
-    </form>
-    <form method="POST" action="{{ url('/posts/pu/edit') }}">
-        @csrf
-        @method('PUT')
-        <input type="text" name="put_input" placeholder="PUT>
-        <button type="submit">Yuborish</button>
-    </form>
-
-
-
+<form method="GET" action="{{ route('posts.index') }}">
+    @csrf
+    <input type="text" name="get_input" placeholder="GET">
+    <button type="submit">Yuborish</button>
+</form>
+<form method="POST" action="{{ route('posts.create') }}">
+    @csrf
+    <input type="text" name="post_input" placeholder="POST">
+    <button type="submit">Yuborish</button>
+</form>
+<form method="POST" action="{{ route('posts.delete') }}">
+    @csrf
+    @method('DELETE')
+    <input type="text" name="delete_input" placeholder="DELETE">
+    <button type="submit">Yuborish</button>
+</form>
+<form method="POST" action="{{ route('posts.edit') }}">
+    @csrf
+    @method('PATCH')
+    <input type="text" name="patch_input" placeholder="PATCH">
+    <button type="submit">Yuborish</button>
+</form>
+<form method="POST" action="{{ route('posts.update') }}">
+    @csrf
+    @method('PUT')
+    <input type="text" name="put_input" placeholder="PUT">
+    <button type="submit">Yuborish</button>
+</form>
